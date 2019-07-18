b957e2b49b6f7a7ece9fb9337bdee25dd4335bb3
James Macmillan <jamescodesthings@gmail.com>
2019-07-18 14:17:24 +0100

    bootstrap: install sls-offline, prettier, eslint

--- handler.ts
+++ handler.ts
@@ -1,12 +1,23 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const hello: APIGatewayProxyHandler = async (
+  event,
+  _context,
+): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2,
+    ),
   };
-}
+};
