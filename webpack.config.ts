9e05de0a9f01359ad0dd26275a69d2bdbcbabe43
James Macmillan <jamescodesthings@gmail.com>
2019-07-18 15:58:20 +0100

    bootstrap: if in doubt, do crazy things

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import webpack from 'webpack';
+import slsw from 'serverless-webpack';
+
+const config: webpack.Configuration = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+};
+
+module.exports = config;
